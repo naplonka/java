b8fac0bd6ceda8df161c3001887351b8755ca6f8
Natalia Rychlowska <natalia.rychlowska@jit.team>
2019-01-15 08:33:17 +0100

    first commit

---
+++ Country.java
@@ -0,0 +1,9 @@
+class Country {   
+	void comingSoon(){
+	System.out.println ("coming soon");
+	 }
+	public static void main(String[] args){
+			Country poland = new Country();
+			poland.comingSoon();
+	}
+}
